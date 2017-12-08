cece918b4e6309ded062d3153b694b0bddec8052
Raulsalmeida <raulnara77@gmail.com>
2017-12-08 19:15:02 -0300

    Atualizações de Biblioteca e Usuario, correção de bugs

--- src/controller/Biblioteca.java
+++ src/controller/Biblioteca.java
@@ -12,8 +12,8 @@ public class Biblioteca {
 	private ArrayList<Livro> livros = new ArrayList <Livro> ();
 	private ArrayList<Reserva> reservas = new ArrayList <Reserva> ();
 	
-	public void cadastrarUsuario(String matricula, String nome, String curso, String senha) {
-		Usuario u = new Usuario(matricula, nome, curso, senha);
+	public void cadastrarUsuario(String matricula, String nome, String cursoUsuario, String senha) {
+		Usuario u = new Usuario(matricula, nome, cursoUsuario, senha);
 		usuarios.add(u);
 	}
 	
